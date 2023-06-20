b57059980008f316f7f18b16be5272c7d4ec586a
XackiGiFF <xackigiff@mc-mpe.ru>
2023-06-21 02:04:37 +0300

    UPD

--- start-mpe_1.20.0.cmd
+++ start-mpe_1.20.0.cmd
@@ -1,3 +1,3 @@
 @echo off
-TITLE MPE-Lobby Server Minecraft Bedrock
+TITLE MPE-Server2 (v1.20.0) Minecraft Bedrock
 docker run -it --rm --name mcpe-1.20.0 --privileged -p 19136:19136 -p 19136:19136/udp -p 19137:19137 -p 19137:19137/udp -v .\mpe_1.20.0\:/home/mcpe/mpe_1.20.0 xackigiff/mcpe-php-8:latest /home/mcpe/mpe_1.20.0/start.sh -l
